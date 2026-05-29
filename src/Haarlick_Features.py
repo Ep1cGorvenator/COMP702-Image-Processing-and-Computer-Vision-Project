9e21a54dbc5292348eb2155a9ee6184cf19a1715
Ep1cGorvenator <dgovender687@gmail.com>
2026-05-29 22:07:41 +0200

    sohil version of haralick and knn

--- src/Haarlick_Features.py
+++ src/Haarlick_Features.py
@@ -1,45 +1,56 @@
 import cv2
 import math
+import mahotas
 import skimage as ski
 import numpy as np
+import sys
 
-image = cv2.imread('dataset/raw/R10/new/front/R10_new_front_001.jpg')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-thresh_gauss = cv2.adaptiveThreshold(
-    gray_image, 255,
-    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY,
-    199, 5
-)
+#image = cv2.imread('dataset/raw/R10/new/front/R10_new_front_001.jpg')
+#gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-thresh_gauss=cv2.bitwise_not(thresh_gauss)
+#thresh_gauss = cv2.adaptiveThreshold(
+#    gray_image, 255,
+#    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+#    cv2.THRESH_BINARY,
+#    199, 5
+#)
+
+#thresh_gauss=cv2.bitwise_not(thresh_gauss)
 
 def getRegions(thresh_img):
+    
     contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     regions = []
     dims = []
     indices = []
     count=0
+#    if(len(contours)==1):
+#        cv2.imshow("window", thresh_img)
+#        cv2.waitKey(0)
+#        sys.exit()
+    
+ #   s_contours = sorted(contours, key = cv2.contourArea, reverse=True)
+
     for contour in contours:
         count+=1
         x, y, w, h = cv2.boundingRect(contour)
         area = cv2.contourArea(contour)
     
-        if area > 500:
+        if area > 750:
             dims.append([w,h])
             indices.append(count)
             region = thresh_img[y:y+h, x:x+w]
-            regions.append(np.array(region))
+            regions.append(np.asarray(region))
             count+=1
 
     return regions,dims,indices
 
 
-regions,dims,indices = getRegions(thresh_gauss)
+#regions,dims,indices = getRegions(thresh_gauss)
 
-print(np.max(regions[17]))
+#print(np.max(regions[17]))
 #print(max(dims))
 #print(len(indices))
 
@@ -61,41 +72,104 @@ def getHaarlickFeatures(regions):
 
         feature = []
     
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='contrast')))
-
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='dissimilarity')))
-
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='homogeneity')))
-
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
+     #   GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='contrast')))
+    #  feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='dissimilarity')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='homogeneity')))
      #   print(GLCM[:,:,1,1])
      #   print(ski.feature.graycoprops(GLCM, prop='energy'))
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='energy')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='energy')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='correlation')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='ASM')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='mean')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='variance')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='std')))
+     #   feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='entropy')))
+    
+        textures = mahotas.features.haralick(region)
+   #     print(np.array(textures.mean(axis=0).shape))
+        features.append(np.asarray(textures.mean(axis=0)))
+        
+    #    features.append(np.array(feature))
+    features=np.asarray(features)
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='correlation')))
+    return features.flatten()
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='ASM')))
+#print(getHaarlickFeatures(regions)[1])
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='mean')))
+def haarlickTrain(train_img_paths):
+    
+    print(len(train_img_paths))
+    training_images_features=[]
+    max_len=0
+    count=0
+    for path_index in range(len(train_img_paths)):
+        count+=1
+        image = cv2.imread(train_img_paths[path_index])
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='variance')))
+        thresh_gauss = cv2.adaptiveThreshold(
+            gray_img, 255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY,
+            199, 5
+        )
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='std')))
+        thresh_gauss=cv2.bitwise_not(thresh_gauss)
 
-        GLCM = ski.feature.graycomatrix(region,[1,1,1,2],[math.pi/4,math.pi/2,3*math.pi/4,0],256,1)
-        feature.append(np.mean(ski.feature.graycoprops(GLCM, prop='entropy')))
+    #    print(train_img_paths[path_index])
+        
+        regions,_,_ = getRegions(thresh_gauss)
 
-        features.append(np.array(feature))
+        imageFeatures=getHaarlickFeatures(regions)
 
-    return features
+        if(len(imageFeatures)>max_len):
+            max_len=len(imageFeatures)
 
-print(getHaarlickFeatures(regions)[1])
+        training_images_features.append(np.asarray(imageFeatures))
+        
+        print(count)
+    
+    for imageFeat in training_images_features:
+        if(len(imageFeat)<max_len):
+            np.pad(imageFeat, (0,len(max_len-imageFeat)), mode='constant', constant_values=0)
 
+    return training_images_features
+
+
+def haarlickTest(test_img_paths):
+    
+    test_images_features=[]
+    print(len(test_img_paths))
+    count=0
+    max_len=0
+    for path_index in range(len(test_img_paths)):
+        count+=1
+        image = cv2.imread(test_img_paths[path_index])
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        thresh_gauss = cv2.adaptiveThreshold(
+            gray_img, 255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY,
+            199, 5
+        )
+
+        thresh_gauss=cv2.bitwise_not(thresh_gauss)
+
+        regions,_,_ = getRegions(thresh_gauss)
+
+        imageFeatures=getHaarlickFeatures(regions)
+
+        if(len(imageFeatures)>max_len):
+            max_len=len(imageFeatures)
+
+        test_images_features.append(np.asarray(imageFeatures))
+        print(count)
+    
+
+    for imageFeat in test_images_features:
+        if(len(imageFeat)<max_len):
+            np.pad(imageFeat, (0,len(max_len-imageFeat)), mode='constant', constant_values=0)
+            
+    return test_images_features
--- src/knn.py
+++ src/knn.py
@@ -19,6 +19,8 @@ from pathlib import Path
 
 import sift_features as sf
 
+import Haarlick_Features as haar
+
 def image_to_feature_vector(image, size=(32, 32)):
 	# resize the image to a fixed size, then flatten the image into
 	# a list of raw pixel intensities
@@ -91,7 +93,7 @@ for (i, imagePath) in enumerate(imagePaths):
 	# show an update every 1,000 images
 	if i > 0 and i % 1000 == 0:
 		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
-		
+"""		
 # show some information on the memory consumed by the raw images
 # matrix and features matrix
 rawImages = np.array(rawImages)
@@ -106,7 +108,9 @@ print("features matrix: {:.2f}MB".format(
 	features.nbytes / (1024 * 1000.0)))
 print("Hu Moments matrix: {:.2f}MB".format(
 	HUMoments.nbytes / (1024 * 1000.0)))
+"""
 
+"""
 #---------TRAIN TEST SPLIT----------------
 #Using raw pixel intensities as features for training and testing
 (trainRI, testRI, trainRL, testRL) = train_test_split(
@@ -125,7 +129,18 @@ print("Hu Moments matrix: {:.2f}MB".format(
 	imagePaths, labels, test_size=0.2, random_state=42)
 sift_x_train, vocabulary = sf.sift_train(trainImagePaths, 100, 200)
 sift_x_test = sf.sift_test(testImagePaths, vocabulary)
-
+"""
+#Haarlick features
+(trainImgPaths, testImgPaths , trainLabelsHaar, testLabelsHaar) = train_test_split(
+	imagePaths, labels, test_size=0.2, random_state=42)
+haarlick_train= haar.haarlickTrain(trainImgPaths)
+#print("Training",haarlick_train.shape)
+#print("TrainLabels", trainLabelsHaar)
+haarlick_test = haar.haarlickTest(testImgPaths)
+#print("Testing",haarlick_test.shape)
+#print("TestingLabels",testLabelsHaar)
+print("done")
+"""
 #----------------KNN CLASSIFICATION----------------
 print("\n-------------------KNN CLASSIFICATION-------------------")
 #YOU CAN SPECIFY HOW MANY NEIGHBOURS TO USE WITH THE n_neighbors PARAMETER, 
@@ -151,7 +166,14 @@ model = KNeighborsClassifier(n_neighbors=1,n_jobs=4)
 model.fit(trainHM, trainLabelsHM)
 acc = model.score(testHM, testLabelsHM)
 print("Hu Moments accuracy: {:.2f}%".format(acc * 100))
-
+"""
+#HAARLICK FEATURES
+print("\nevaluating Haarlick features accuracy:")
+model = KNeighborsClassifier(n_neighbors=1,n_jobs=4)
+model.fit(haarlick_train, trainLabelsHaar)
+acc = model.score(haarlick_test, testLabelsHaar)
+print("Haarlick accuracy: {:.2f}%".format(acc * 100))
+"""
 #----------------NAIVE BAYES CLASSIFICATION----------------
 print("\n\n-------------------NAIVE BAYES CLASSIFICATION-------------------")
 #RAW PIXEL FEATURES
@@ -209,3 +231,4 @@ pipe.fit(sift_x_train, siftTrainLabels)
 pipe.score(sift_x_test, siftTestLabels)
 print(classification_report(siftTestLabels, pipe.predict(sift_x_test)))
 
+"""
